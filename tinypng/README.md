3229bba67631c55be546859b8112388048f5209e
fqxyi <fengqingxiuyi@gmail.com>
2017-11-12 01:16:50 +0800

    tinypng client

---
+++ tinypng/README.md
@@ -0,0 +1,25 @@
+# TinyPng
+
+一个使用TinyPng提供的API，用于Windows或Mac端快速压缩图片的Python脚本，可以做到将该目录下所有的图片进行压缩，包括子目录中的图片。
+
+# 背景
+
+1、 TinyPng有Mac应用，下载地址：[TinyPNG4Mac](https://github.com/kyleduo/TinyPNG4Mac/releases)；
+
+2、 TinyPng没有Windows应用，网上流传的大多都是通过Python脚本实现，但是使用起来总有各种各样的问题，总结发现主要有以下两点原因：
+
+1. Python版本导致，本文使用的Python版本是3.5.0，但并不表示其他Python版本不可使用，由于个人原因没有测试过；
+2. 业务需求导致，再加上对Python代码不熟悉，修改Python脚本的难度较大，本文对于自己写的Python脚本进行了非常详细的注释，如果你想了解更多Python相关的知识，请自行谷歌、百度。
+
+# 注意事项
+
+1. 本文编写的tinypng_mac.py脚本由于个人条件没有实际测试过，如有问题请自行解决；
+2. 如果是由于业务需求导致无法实现你想要的功能，您可以提issue，互相交流；
+3. 如果你是免费用户，那么每个developer key最多只能压`500`次，可通过多注册几个邮箱的方式解决次数的限制。
+
+# 使用教程
+
+1. 安装Python，Mac系统自带，Windows电脑可通过[官网](https://www.python.org/downloads/)下载；
+2. 在终端或CMD窗口中输入`python -V`命令，校验Python是否正确安装；
+3. 在终端或CMD窗口中输入`pip install -i https://pypi.doubanio.com/simple/ tinify`命令，使用国内镜像下载安装tinify；
+4. 在终端或CMD窗口中输入`python tinypng_mac.py`或`python tinypng_win.py`命令，进行图片批量压缩。
---
+++ tinypng/tinypng_win.py
@@ -0,0 +1,42 @@
+#-*- coding:utf-8 -*-
+
+# 说明
+## 1. Developer API Key地址：进入https://tinypng.com/dashboard/developers网址后选择Developer API选项卡
+## 2. API地址：https://tinypng.com/developers/reference/python
+## 3. 免费用户每个月最多只能压500次，可通过多注册几个邮箱的方式解决次数的限制
+## 4. 仅Windows电脑才能正确使用该脚本
+
+import tinify
+import os
+import os.path
+
+tinify.key ="Your Developer API Key" # AppKey
+fromPath ="C:\\Users\\Administrator\\Desktop\\temp\\src" # source dir path
+toPath ="C:\\Users\\Administrator\\Desktop\\temp\\dest" # dest dir path
+
+# root, dirs, files参数的含义：目录的路径(String)；root目录下所有子目录的名字(List)；root目录下非目录的名字
+# os.walk(top,topdown=True,onerror=None)函数中各参数的含义：需要遍历的顶级目录的路径；默认值是“True”表示首先返回顶级目录下的文件，然后再遍历子目录中的文件；默认值为"None"，表示忽略文件遍历时的错误
+for root, dirs, files in os.walk(fromPath):
+	newToPath = toPath
+	if len(root) > len(fromPath): # 比较root和fromPath的字符长度
+		innerPath= root[len(fromPath):] # 字符串切割，将root字符串从第len(fromPath)个位置开始截取，不包括len(fromPath)这个位置的字符
+		if innerPath[0] == '\\': # 判断innerPath的第一个字符是不是\符号
+			innerPath = innerPath[1:] # 字符串切割，例如innerPath的值为\test，那么innerPath[1:]之后的值为test
+		newToPath = os.path.join(toPath,innerPath) # 将toPath目录和innerPath文件或文件夹拼接之后的路径赋值给newToPath
+	
+	for name in files:
+		newFromFilePath = os.path.join(root, name)
+		newToFilePath = os.path.join(newToPath, name)
+		fileName, fileSuffix = os.path.splitext(name) # 分解文件名的扩展名
+		if fileSuffix == '.png' or fileSuffix == '.jpg':
+			source = tinify.from_file(newFromFilePath)
+			source.to_file(newToFilePath)
+		else:
+			pass
+
+	for dirName in dirs:
+		if os.path.exists(os.path.join(newToPath, dirName)):
+			pass
+		else:
+			os.mkdir(os.path.join(newToPath, dirName))
+
